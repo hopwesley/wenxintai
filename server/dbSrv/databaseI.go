40e852199d75c7defae5d16f98c1ebf5627c56a4
basdevelop <hyperorchid@outlook.com>
2025-11-18 14:51:26 +0800

    got ai content from db when has generated

--- server/dbSrv/databaseI.go
+++ server/dbSrv/databaseI.go
@@ -22,14 +22,3 @@ type DbService interface {
 	SaveRiasecSession(ctx context.Context, publicId, businessType string, questionsJSON []byte) error
 	UpdateRiasecAnswers(ctx context.Context, publicId string, answersJSON []byte) error
 }
-
-type txKey struct{}
-
-func ContextWithTx(ctx context.Context, tx *sql.Tx) context.Context {
-	return context.WithValue(ctx, txKey{}, tx)
-}
-
-func TxFromContext(ctx context.Context) (*sql.Tx, bool) {
-	tx, ok := ctx.Value(txKey{}).(*sql.Tx)
-	return tx, ok
-}
